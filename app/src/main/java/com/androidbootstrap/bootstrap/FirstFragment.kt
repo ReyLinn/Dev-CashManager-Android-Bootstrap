cd830a48d01b3d471dbacaba5d4e4a593fa92a47
Lucas Boisbourdin <m.boisbourdin@gmail.com>
2020-12-04 23:07:48 +0100

    Scanner Done

--- app/src/main/java/com/androidbootstrap/bootstrap/FirstFragment.kt
+++ app/src/main/java/com/androidbootstrap/bootstrap/FirstFragment.kt
@@ -1,22 +1,28 @@
 package com.androidbootstrap.bootstrap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-private const val CAMERA_REQUEST_CODE = 101
 class FirstFragment : Fragment() {
-
     private lateinit var codeScanner: CodeScanner
-
+    private lateinit var scannerView: CodeScannerView
+    private lateinit var textViewScanner: TextView
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -27,8 +33,44 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+        textViewScanner = view.findViewById<TextView>(R.id.textview_first)
+        codeScanner()
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun codeScanner(){
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+            decodeCallback = DecodeCallback {
+                activity.runOnUiThread() {
+                    textViewScanner.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                activity.runOnUiThread {
+                    Log.e("Main", "Camera initialisation error: ${it.message}")
+                }
+            } }
     }
 }
--- app/src/main/java/com/androidbootstrap/bootstrap/MainActivity.kt
+++ app/src/main/java/com/androidbootstrap/bootstrap/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.androidbootstrap.bootstrap
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 
+private const val CAMERA_REQUEST_CODE = 101
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
+        setUpPermissions()
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
@@ -33,6 +46,33 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setUpPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need the camera permission to use this app", Toast.LENGTH_SHORT).show()
+                }else{
+                    //successful
+                }
+            }
         }
     }
 }
